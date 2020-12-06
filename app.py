fa6befe05423cbe5b75860958ec075748269681f
Nikita Yasinski <nikita.yasinski43@gmail.com>
2020-12-06 17:01:29 +0300

    added crud for notes

--- app.py
+++ app.py
@@ -79,29 +79,78 @@ def manage_user(id):
         conn.commit()
         return json.dumps({'message': 'success'})
 
-@app.route('/users/<int:user_id>/notes', methods=['GET'])
-def get_note(user_id):
-    conn = mysql.connect()
-    cur = conn.cursor()
-    resultValue = cur.execute(f'''
-    SELECT user_list.id, user_list.name, user_notes.id, user_notes.title, user_notes.content FROM user_list 
-    INNER JOIN user_notes ON user_list.id = user_notes.user_id 
-    WHERE user_list.id={user_id}
-    ''')
-    user2 = cur.fetchall()
+@app.route('/users/<int:user_id>/notes', methods=['GET', 'POST'])
+def manage_notes(user_id):
     
-    data = {}
-    keys = ('user_id', 'user_name', 'note_id', 'title', 'content')
-    for i in range(len(user2)):
-        result = {}
-        for k, v in zip(keys, user2[i]):
-            result[k] = v
-        data[f'note {i + 1}'] = result
-    return json.dumps(data, indent=4)
+    if request.method == 'GET':
+        conn = mysql.connect()
+        cur = conn.cursor()
+        cur.execute(f'''
+        SELECT user_list.id, user_list.name, user_notes.id, user_notes.title, user_notes.content FROM user_list 
+        INNER JOIN user_notes ON user_list.id = user_notes.user_id 
+        WHERE user_list.id={user_id}
+        ''')
+        user2 = cur.fetchall()
+    
+        data = {}
+        keys = ('user_id', 'user_name', 'note_id', 'title', 'content')
+        for i in range(len(user2)):
+            result = {}
+            for k, v in zip(keys, user2[i]):
+                result[k] = v
+            data[f'note {i + 1}'] = result
+        return json.dumps(data, indent=4)
 
-@app.route('/')
-def index():
-    return 'Hello World!'
+    if request.method == 'POST':
+        conn = mysql.connect()
+        cur = conn.cursor()
+        note_title = request.json['title']
+        note_content = request.json['content']
+        cur.execute(f'''
+        INSERT INTO user_notes(title, content, user_id) VALUES ('{note_title}', '{note_content}', '{user_id}')
+        ''')
+        conn.commit()
+        return json.dumps({'message': 'success'})
+
+@app.route('/users/<int:user_id>/notes/<int:note_id>', methods=['GET', 'PUT', 'DELETE'])
+def manage_note(user_id, note_id):
+    
+    if request.method == 'GET':
+        conn = mysql.connect()
+        cur = conn.cursor()
+        cur.execute(f'''
+        SELECT user_list.id, user_list.name, user_notes.id, user_notes.title, user_notes.content FROM user_list 
+        INNER JOIN user_notes ON user_list.id = user_notes.user_id 
+        WHERE user_list.id={user_id} AND user_notes.id={note_id}
+        ''')
+        user3 = cur.fetchall()
+    
+        data = {}
+        keys = ('user_id', 'user_name', 'note_id', 'title', 'content')
+        for i in range(len(user3)):
+            result = {}
+            for k, v in zip(keys, user3[i]):
+                result[k] = v
+            data[f'note {i + 1}'] = result
+        return json.dumps(data, indent=4)
+    
+    if request.method == 'DELETE':
+        conn = mysql.connect()
+        cur = conn.cursor()
+        cur.execute(f"DELETE FROM user_notes WHERE id={note_id} AND user_id={user_id}")
+        conn.commit()
+        return json.dumps({'message': 'success'})
+
+    if request.method == 'PUT':
+        conn = mysql.connect()
+        cur = conn.cursor()
+        note_title = request.json['title']
+        note_content = request.json['content']
+        cur.execute(f'''
+        UPDATE user_notes SET title='{note_title}', content='{note_content}' WHERE id={note_id} AND user_id={user_id}
+        ''')
+        conn.commit()
+        return json.dumps({'message': 'success'})
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True, host='0.0.0.0')
